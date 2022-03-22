5f505ae9f3f590e9267d2c74062cefc574f4cd9f
mmerihsesh <84874492+mmerihsesh@users.noreply.github.com>
2022-03-22 23:55:01 +0300

    Update Stage_1.py

--- Stage_1.py
+++ Stage_1.py
@@ -1,7 +1,13 @@
 msg_0 = "Enter an equation"
+msg_1 = "Do you even know what numbers are? Stay focused!"
+msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 
-calc = input(msg_0)
-x, oper, y = calc.split()
+calc = input()
+def stage_1(calc):
+    x, oper, y = calc.split()
+    print(x, oper, y)
+
+stage_1(calc)
 
 try:
     x, y = float(x), float(y)
@@ -12,8 +18,3 @@ if oper == '+' or oper == '-' or oper == '*' or oper == '/':
     print('ok')
 else:
     print('ne ok')
-
-
-msg_1 = "Do you even know what numbers are? Stay focused!"
-
-msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
