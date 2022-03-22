ca3b885f6821e4e0830e2ff55fd3f029b4c767a6
mmerihsesh <84874492+mmerihsesh@users.noreply.github.com>
2022-03-23 02:32:41 +0300

    Update Stage_1.py

--- Stage_1.py
+++ Stage_1.py
@@ -2,19 +2,19 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 
-calc = input()
-def stage_1(calc):
-    x, oper, y = calc.split()
-    print(x, oper, y)
 
-stage_1(calc)
+def test_func():
+    while True:
+        print(msg_0)
+        try:
+            x, oper, y = input().split()
+            float(x)
+            float(y)
+        except (TypeError, ValueError):
+            print(msg_1)
+        else:
+            if oper in {'+', '-', '/', '*'}:
+                break
+            print(msg_2)
 
-try:
-    x, y = float(x), float(y)
-except ValueError:
-    print('ValueError')
-
-if oper == '+' or oper == '-' or oper == '*' or oper == '/':
-    print('ok')
-else:
-    print('ne ok')
+test_func()
