210f5b9db984f08b6d45dcf90f90764c0f20997d
mmerihsesh <84874492+mmerihsesh@users.noreply.github.com>
2022-03-23 22:30:03 +0300

    Update Stage_1.py

--- Stage_1.py
+++ Stage_1.py
@@ -1,6 +1,8 @@
 msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
+msg_3 = "Yeah... division by zero. Smart move..."
+result = 'not set'
 
 
 def test_func():
@@ -8,13 +10,27 @@ def test_func():
         print(msg_0)
         try:
             x, oper, y = input().split()
-            float(x)
-            float(y)
+            x = float(x)
+            y = float(y)
         except (TypeError, ValueError):
             print(msg_1)
         else:
             if oper in {'+', '-', '/', '*'}:
-                break
+                if oper == '+':
+                    result = x + y
+                elif oper == '-':
+                    result = x - y
+                elif oper == '*':
+                    result = x * y
+                elif oper == '/' and y != 0:
+                    result = x / y
+                else:
+                    print(msg_3)
+                    continue
+                if result != 'not set':
+                    print(result)
+                    break
             print(msg_2)
+
 
 test_func()
