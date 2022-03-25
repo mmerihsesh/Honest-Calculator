8398d387b368ee7ff8288599edf760683ba26fd6
mmerihsesh <84874492+mmerihsesh@users.noreply.github.com>
2022-03-26 02:12:33 +0300

    Update Stage_1.py

--- Stage_1.py
+++ Stage_1.py
@@ -2,16 +2,35 @@ msg_0 = "Enter an equation"
 msg_1 = "Do you even know what numbers are? Stay focused!"
 msg_2 = "Yes ... an interesting math operation. You've slept through all classes, haven't you?"
 msg_3 = "Yeah... division by zero. Smart move..."
+msg_4 = "Do you want to store the result? (y / n):"
+msg_5 = "Do you want to continue calculations? (y / n):"
 result = 'not set'
 
 
+def last_func():
+    while True:
+        print(msg_5)
+        answer = input()
+        if answer == 'y':
+            return
+        elif answer == 'n':
+            return answer
+
+
 def test_func():
+    memory = 0
     while True:
         print(msg_0)
         try:
             x, oper, y = input().split()
-            x = float(x)
-            y = float(y)
+            if x != 'M':
+                x = float(x)
+            else:
+                x = memory
+            if y != 'M':
+                y = float(y)
+            else:
+                y = memory
         except (TypeError, ValueError):
             print(msg_1)
         else:
@@ -29,8 +48,22 @@ def test_func():
                     continue
                 if result != 'not set':
                     print(result)
-                    break
+                    while True:
+                        print(msg_4)
+                        answer = input()
+                        if answer == 'y':
+                            memory = result
+                            answer = last_func()
+                            break
+                        elif answer == 'n':
+                            answer = last_func()
+                            break
+                        break
+                    if answer == 'n':
+                        break
+                    continue
             print(msg_2)
 
 
 test_func()
+
